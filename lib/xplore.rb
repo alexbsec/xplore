189efc2c78d993f4c6844bc201d648393f0b849e
Joao Buschinelli <j.a.c.buschinelli@gmail.com>
2023-05-15 11:41:44 -0300

    added -o flag to save outputs to file passed. EX: -o file will salve to file-{status-code}.txt files

--- lib/xplore.rb
+++ lib/xplore.rb
@@ -9,12 +9,15 @@ def main()
     flags[:gcode] = 200
 
     OptionParser.new do |opts|
-        opts.banner = "Usage: prog.rb [options] [php|asp|aspx|html|config|ui_config|xml|simple|js|routes] [flags] [-u url | -g grep status code | -s small wordlist]"
+        opts.banner = "Usage: prog.rb [options] [php|asp|aspx|html|config|ui_config|xml|simple|js|routes] [flags] [-u url | -g grep status code | -s small wordlist | -o save-output]"
         opts.on("-u URL", '--url URL', "Specify the target URL") do |url|
             flags[:url] = url
         end
         opts.on("-g STATUS-CODE", '--grep-code STATUS-CODE', "Specify which status code should not be ignored. Default is 200 (can be a list).") do |gcode|
             flags[:gcode] = gcode
+        end
+        opts.on("-o SAVE-OUTPUT", '--output', "Set this flag with a name to save the results in a .txt file.") do |output|
+            flags[:output] = output
         end
         opts.on("-s", "--small", TrueClass, "Set the scan to use small wordlist. Default is false.")
     end.parse!(into: flags)
@@ -33,6 +36,7 @@ def main()
     end
 
     url = flags[:url]
+    output = flags[:output]
 
     if url.nil?
         puts "Missing url flag (-u)".red
@@ -52,7 +56,7 @@ def main()
         gcode = [200]
     end
 
-    xplore = Xplore.new(url, gcode, mode, "small")
+    xplore = Xplore.new(url, gcode, mode, "small", output)
     puts %q{
         __    __            __                               
         /  |  /  |          /  |                              
@@ -67,8 +71,9 @@ def main()
                   $$ |                                        
                   $$/         
 }.red
-    puts "Starting to \033[1mXplore\033[0m".center(80, '=')
+    puts "Starting to \033[1mXplore\033[0m".center(120, '=')
     xplore.request
+    xplore.process_output_files
 
 end
 
