ec8f53f630b14f6cdd2c5dd0e792cdc5a261d2d5
Joao Buschinelli <j.a.c.buschinelli@gmail.com>
2023-05-14 22:04:38 -0300

    Argparse working

--- lib/xplore.rb
+++ lib/xplore.rb
@@ -2,30 +2,44 @@ require 'net/http'
 require 'optparse'
 
 
-options = {}
-options[:url] = ''
 
-parser = OptionParser.new do |opts|
-    opts.banner = "Usage: prog.rb [options] [php|asp|aspx|html|config|ui_config|massive|general|simple|fast|js|routes] [flags] [-u url]"
-    opts.on("-u URL", '--url URL', "Specify the target URL") do |url|
-        options[:url] = url
+def main()
+    flags = {}
+    positional_arg_list = %w[php asp aspx html config ui_config massive general simple js routes]
+    
+    OptionParser.new do |opts|
+        opts.banner = "Usage: prog.rb [options] [php|asp|aspx|html|config|ui_config|massive|general|simple|js|routes] [flags] [-u url | -g grep status code | -s small wordlist]"
+        opts.on("-u URL", '--url URL', "Specify the target URL") do |url|
+            flags[:url] = url
+        end
+        opts.on("-g STATUS-CODE", '--grep-code STATUS-CODE', "Specify which status code should not be ignored. Default is 200 (can be a list).") do |gcode|
+            flags[:gcode] = gcode
+        end
+        opts.on("-s", "--small", TrueClass, "Set the scan to use small wordlist. Default is false.")
+    end.parse!(into: flags)
+    
+    
+    if ARGV.empty?
+        puts "Please specify a positional argument: [php|asp|aspx|html|config|ui_config|massive|general|simple|fast|js|routes]"
+        exit
     end
+    
+    mode = ARGV[0]
+    
+    if mode && !positional_arg_list.include?(mode)
+        puts "Invalid option: #{mode}. Please use one of the following: #{positional_arg_list}"
+        exit
+    end
+
+    url = flags[:url]
+    use_small = flags[:small]
+    gcode = flags[:gcode]
+    if use_small.nil?
+        use_small = false
+    end
+
+
+
 end
 
-parser.parse!
-
-if ARGV.empty?
-    puts "Please specify a positional argument: [php|asp|aspx|html|config|ui_config|massive|general|simple|fast|js|routes]"
-    exit
-end
-
-positional_arg = ARGV[0]
-
-puts "Positional argument: #{positional_arg}"
-puts "URL: #{options[:url]}"
-
-def get_status_code(url)
-    uri = URI(url)
-    response = Net::HTTP.get_response(uri)
-    response.code.to_i
-end
+main()
